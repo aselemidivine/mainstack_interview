b695b31730f80e8e8e26e08a95b972a811b78e58
aselemidivine <aselemidivine@gmail.com>
2024-04-10 06:58:48 +0100

    finished implementing the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
-import { TfiCrown } from "react-icons/tfi";
+import React, { } from "react";
 import Marketing from "../assets/images/widgets.svg";
 import Analytics from "../assets/images/analytics.png";
 import Home from "../assets/images/home.svg";
 import Crm from "../assets/images/crm.svg";
 import Revenue from "../assets/images/payments.png";
+import Notification from "../assets/images/notifications.svg";
+import Menu from "../assets/images/menu.svg";
+import Message from "../assets/images/icon.svg";
+import User from "../assets/images/user.svg";
 import "../assets/css/navbar.css";
 // import { HOST_URL } from "../assets/js/help_func";
 
@@ -48,10 +51,26 @@ function Navbar() {
                 <p>Apps</p>
               </li>
             </ul>
+            <div className="menu__items--right">
+              <div>
+                <img className="menu__items--img--main" src={Notification} alt="" />
+              </div>
+              <div>
+                <img className="menu__items--img" src={Message} alt="" />
+              </div>
+              <div>
+                <div></div>
+                <div className="user__icon">
+                <img className="menu__items--img--user" src={User} alt="" />
+                <img className="menu__items--img--user" src={Menu} alt="" />
+
+                </div>
+              </div>
+            </div>
           </nav>
         </div>
       </header>
-      {/* MOBILE HEADER */}
+      
     </>
   );
 }
